dd6bcdeb4167c0c69a128a4e94843011564a7b82
eimaam <imamddahir@gmail.com>
2026-08-03 08:07:39 +0100

    fix api

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LoggingInterceptor(), new ResponseInterceptor());
 
-  await app.listen(AppConfig.PORT);
+  await app.listen(AppConfig.PORT, '0.0.0.0');
   console.log(`Server running on http://localhost:${AppConfig.PORT}/api/v1`);
 }
 bootstrap();
